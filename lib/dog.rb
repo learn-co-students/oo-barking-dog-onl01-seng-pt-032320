50959ff516ef2476fb17134c4bc30986eabe59d3
Dina Kendle <dina.kendle@gmail.com>
2020-04-05 13:58:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog 
+  
+  def name=(dog_name)
+    @this_is_the_dogs_name = dog_name
+  end
+  
+  def name
+    @this_is_the_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end  
+
+steve = Dog.new
+steve.name = "Steve"
+steve.name
+
+steve.bark
