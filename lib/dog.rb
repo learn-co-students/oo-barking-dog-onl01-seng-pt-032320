39c9344f7f625487b8a222deaf204be7226f5301
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-10 01:58:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @name_for_dog = dog_name
+  end 
+  
+  def name 
+    @name_for_dog
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end 
