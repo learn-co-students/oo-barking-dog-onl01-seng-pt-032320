a2ff14b275df4dd8500cc4ca13d3398f125cf950
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-16 17:27:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+#
+# Dog
+#   instantiates a new dog
+#   has a name
+#   is able to bark
+
+class Dog
+  def name=(name)
+    @dogs_name = name
+  end
+
+  def name
+    @dogs_name
+  end
+
+  def  bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.name
+
+fido.bark
